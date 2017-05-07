3ed12b18ec489cbc569d079198f1027594c996aa
Ryan Muskopf <ryanjmuskopf@gmail.com>
2017-05-07 12:21:41 -0400

    fixed a return value

--- server/router.js
+++ server/router.js
@@ -3,17 +3,17 @@ const mid = require('./middleware');
 
 const router = (app) => {
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
-  //app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
-  //app.get('/getDogos', mid.requiresLogin, controllers.Dogo.getDogos);
+  // app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
+  // app.get('/getDogos', mid.requiresLogin, controllers.Dogo.getDogos);
   app.get('/getBaseStats', mid.requiresLogin, controllers.Account.getBaseStats);
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  //app.get('/dogoMaker', mid.requiresLogin, controllers.Dogo.makerPage);
-  //app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
-  //app.post('/dogoMaker', mid.requiresLogin, controllers.Dogo.make);
-  //app.post('/maker', mid.requiresLogin, controllers.Domo.make);
+  // app.get('/dogoMaker', mid.requiresLogin, controllers.Dogo.makerPage);
+  // app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  // app.post('/dogoMaker', mid.requiresLogin, controllers.Dogo.make);
+  // app.post('/maker', mid.requiresLogin, controllers.Domo.make);
   app.get('/clicker', mid.requiresLogin, controllers.Clicker.clickerPage);
   app.post('/clicker', mid.requiresLogin, controllers.Account.updateValues);
   app.get('/store', mid.requiresLogin, controllers.Store.storePage);
